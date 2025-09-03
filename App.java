c69b30b8ce26ce85672b05dc194ec326bde238b9
golihaidari <golihaidari@gmail.com>
2025-09-03 09:49:02 +0200

    change code

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello world!");    
+        System.out.println("hello world!"); 
+        int x =3;
+        while (x !=0){
+            x--;
+        }   
+        System.out.println("x: " + x);
     }
 }
